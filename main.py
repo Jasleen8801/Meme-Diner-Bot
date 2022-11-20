3c9bf778b1c59020124f15087574e7f84bea407e
Jasleen Kaur <91721589+Jasleen8801@users.noreply.github.com>
2022-11-21 00:07:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from dotenv import load_dotenv
 import random
 import json
 
-# load_dotenv()
+load_dotenv()
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-GUILD_ID = os.getenv("GUILD_ID")
+GUILD_ID = 1028619109044326440
 
 bot = commands.Bot(intents=nextcord.Intents.all())
 
@@ -19,14 +19,14 @@ async def on_ready():
 
 
 @bot.slash_command(name="meme", guild_ids=[GUILD_ID], description="To get a random food related meme")
-async def meme(ctx):
+async def meme(interaction):
     fp = open("links.json")
     data = json.load(fp)
     img_url = random.choice(data['links'])
     embed = nextcord.Embed()
     embed.set_image(url=img_url)
     fp.close()
-    await ctx.respond(embed=embed)
+    await interaction.send(embed=embed)
 
 
 bot.run(DISCORD_TOKEN)
