e20c4bdeb77b5df369258ddccd1520f5350b69fe
Jasleen Kaur <91721589+Jasleen8801@users.noreply.github.com>
2022-11-20 23:58:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import discord
-from discord.ext import commands
+import nextcord
+from nextcord.ext import commands
 import os
 from dotenv import load_dotenv
 import random
@@ -10,12 +10,12 @@ load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 GUILD_ID = os.getenv("GUILD_ID")
 
-bot = commands.Bot(command_prefix="/", intents=discord.Intents.all())
+bot = commands.Bot(intents=nextcord.Intents.all())
 
 @bot.event
 async def on_ready():
     print("online")
-    return await bot.change_presence(activity=discord.Activity(type=1, name='Foodie Meme Lover'))
+    return await bot.change_presence(activity=nextcord.Activity(type=nextcord.ActivityType.streaming, name='Foodie Memes'))
 
 
 @bot.slash_command(name="meme", guild_ids=[GUILD_ID], description="To get a random food related meme")
@@ -23,7 +23,7 @@ async def meme(ctx):
     fp = open("links.json")
     data = json.load(fp)
     img_url = random.choice(data['links'])
-    embed = discord.Embed()
+    embed = nextcord.Embed()
     embed.set_image(url=img_url)
     fp.close()
     await ctx.respond(embed=embed)
